477c5bb95c0f0e832d425d301a60f65722de0c25
AndreasEdo <andreasedwardputrajatmiko@gmail.com>
2025-05-29 16:42:30 +0700

    footer and header component

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="w-full flex flex-wrap justify-between bg-white mt-8 fixed bottom-0 text-[#F06292] p-2 md:p-4 text-[6px] md:text-lg h-1 md:h-10">
+    <a href="#" class="font-bold">About Us</a>
+    <h1>|</h1>
+    <a href="#" class="font-bold">Contact</a>
+    <h1>|</h1>
+    <a href="#" class="font-bold">FAQ</a>
+    <h1>|</h1>
+    <a href="#" class="font-bold">@2025 Tim meledak Dikit</a>
+</footer>
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,17 @@
+<nav class="w-full flex flex-wrap justify-between bg-white mt-8 p-4">
+  <div class="flex flex-wrap items-center gap-1 md:gap-20">
+    <h1 class="text-[#E91E63] text-3xl md:text-5xl font-bold">Sweet Shop</h1>
+    <div class="flex gap-4 md:gap-20">
+      <a href="/" class="text-[#F06292] text-xl md:text-3xl font-bold">Home</a>
+      <a href="#" class="text-[#F8BBD0] text-xl md:text-3xl font-bold">List Product</a>
+    </div>
+  </div>
+  <div class="flex items-center gap-4 md:gap-6 mt-4 md:mt-0">
+    @if (auth()->check())
+
+    @else
+        <a href="#" class="text-[#F06292] font-medium text-lg md:text-xl">Sign In</a>
+        <a href="#" class="text-white font-medium text-lg md:text-xl px-4 py-2 rounded-full bg-[#E91E63]">Sign Up</a>
+    @endif
+  </div>
+</nav>
