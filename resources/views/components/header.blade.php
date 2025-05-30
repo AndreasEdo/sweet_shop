beb39431e9462c6d424b520e34b414f26285e43b
AndreasEdo <andreasedwardputrajatmiko@gmail.com>
2025-05-30 10:37:40 +0700

    log out button

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -8,6 +8,35 @@
   </div>
   <div class="flex items-center gap-4 md:gap-6 mt-4 md:mt-0">
     @if (auth()->check())
+        <a class="text-[#E91E63] font-bold cursor-pointer">You have {{Auth::user()->money}}$</a>
+        <div class="relative group inline-block">
+        <a
+            href="#"
+            class="flex items-center bg-[#E91E63] text-white px-4 py-2 rounded-l-3xl text-sm sm:text-base font-medium hover:bg-white hover:text-[#E91E63] transition-all duration-300"
+        >
+            <img
+            src="/assets/images/user-circle.png"
+            alt="User Icon"
+            class="w-5 h-5 inline-block mr-2"
+            />
+            {{Auth::user()->name}}
+        </a>
+
+        <div
+            class="absolute left-0 mt-2 w-full bg-white rounded-md shadow-lg opacity-0 group-hover:opacity-100 group-hover:visible invisible transition-all duration-300 z-50"
+        >
+            <ul class="py-2">
+            <li>
+                <a
+                href="#"
+                class="block px-4 py-2 text-sm text-[#E91E63] hover:bg-pink-100 transition"
+                >
+                Log Out
+                </a>
+            </li>
+            </ul>
+        </div>
+        </div>
 
     @else
         <a href="{{route('login_page')}}" class="text-[#F06292] font-medium text-lg md:text-xl">Sign In</a>
