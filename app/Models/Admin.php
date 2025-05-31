adbd5c055b008f25576f874ea56f6aa8e5df22be
AndreasEdo <andreasedwardputrajatmiko@gmail.com>
2025-05-31 23:24:22 +0700

    admin model

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Auth\Middleware\Authenticate;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 
-class Admin extends Model
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Admin extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\AdminFactory> */
     use HasFactory, Notifiable;
